9e21ea042bad87d06e5dad8277b52270506c878a
skf001 <test@skf001.com>
2017-10-31 13:42:46 +0800

    001

---
+++ src/main/java/com/miao/HelloController.java
@@ -0,0 +1,19 @@
+package com.miao;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by skf
+ * Date 2017/10/31.
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String index(){
+        System.out.println("hello world");
+        return "hello world";
+
+    }
+}
